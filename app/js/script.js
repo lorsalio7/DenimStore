1cb4ed59c2069a520ab95dcddac5e07cca11cab8
lorsalio7 <lorsalio7@gmail.com>
2024-09-30 21:34:56 +0500

    Изменить кастомный select на стандартный

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,2 @@
 @@include("parts/site-header-menu.js");
-@@include("parts/dropdown-filter.js");
 @@include("parts/card-gallery.js");
--- dist/js/script.js
+++ dist/js/script.js
@@ -43,50 +43,6 @@ function changeMenuView(width) {
 burgerMenuWidth.onchange = function (e) {
   changeMenuView(e.matches);
 };
-var dropdownFilter = document.querySelector(".dropdown-filter");
-if (dropdownFilter) {
-  var openDropdown = function openDropdown() {
-    dropdownFilter.classList.toggle("dropdown-filter--active");
-    optionList.classList.toggle("dropdown-filter__list--active");
-  };
-  var closeDropdown = function closeDropdown() {
-    dropdownFilter.classList.remove("dropdown-filter--active");
-    optionList.classList.remove("dropdown-filter__list--active");
-  };
-  var optionList = dropdownFilter.querySelector(".dropdown-filter__list");
-  var options = dropdownFilter.querySelectorAll(".dropdown-filter__option");
-  var selectedOption = dropdownFilter.querySelector(".dropdown-filter__text");
-  dropdownFilter.onfocus = function () {
-    document.addEventListener("keydown", function (e) {
-      if (e.code === "Enter") {
-        openDropdown();
-      }
-    });
-  };
-  selectedOption.addEventListener("click", openDropdown);
-  document.addEventListener("click", function (e) {
-    if (e.target !== selectedOption && optionList.classList.contains("dropdown-filter__list--active")) {
-      closeDropdown();
-    }
-  });
-  options.forEach(function (item) {
-    item.addEventListener("click", function (e) {
-      var optionText = e.currentTarget.textContent;
-      dropdownFilter.classList.remove("dropdown-filter--active");
-      optionList.classList.remove("dropdown-filter__list--active");
-      selectedOption.textContent = optionText;
-    });
-    item.onfocus = function () {
-      document.addEventListener("keydown", function (e) {
-        if (e.code === "Enter") {
-          selectedOption.textContent = item.textContent;
-          closeDropdown();
-        }
-      });
-    };
-  });
-}
-;
 var gallerySlider = document.querySelector(".full-card-slider");
 if (gallerySlider) {
   var productGalleryThumbsSlider = new Swiper(".full-card-slider-thumbs", {
